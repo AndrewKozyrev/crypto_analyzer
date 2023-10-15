00a60571c3f79769ab2d8650e950887a6f99e7c3
maria <mariacodes77@gmail.com>
2023-10-15 20:04:19 +0300

    wip

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -1,9 +1,30 @@
 public class CaesarCipher {
     public static final String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя.,”:-!? ";
 
-    public static String encrypt(String text, int key) {return null;}
+    public static String encrypt(String text, int key) {
+        StringBuilder encryptedText = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            int charPosition = alphabet.indexOf(c);
+            int keyVal = (key + charPosition) % alphabet.length();
+            char replaceVal = alphabet.charAt(keyVal);
+            encryptedText.append(replaceVal);
+        }
+        return encryptedText.toString();
+    }
 
-    public static String decrypt(String text, int key) {return null;}
+    public static String decrypt(String text, int key) {
+        StringBuilder decryptedText = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            int charPosition = alphabet.indexOf(c);
+            int keyVal = (charPosition - key + alphabet.length()) % alphabet.length();
+            char replaceVal = alphabet.charAt(keyVal);
+            decryptedText.append(replaceVal);
+        }
+
+        return decryptedText.toString();
+    }
 
     public static String hackCipher(String text) {return null;}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the path to the source text file: ");
+        String path = scanner.nextLine();
+
+        System.out.print("Enter the encryption key: ");
+        int key = scanner.nextInt();
+
+        try {
+            File inputFile = new File(path);
+            File outputFile = new File("encrypted.txt");
+            FileReader reader = new FileReader(inputFile);
+            FileWriter writer = new FileWriter(outputFile);
+
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                String encryptedLine = CaesarCipher.encrypt(line, key);
+                bufferedWriter.write(encryptedLine);
+                bufferedWriter.newLine();
+            }
+            bufferedReader.close();
+            bufferedWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
